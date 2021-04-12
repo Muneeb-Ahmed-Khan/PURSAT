72fb67d8bbc480e6fa804d10479c7f866641ee05
usama932 <usama1517a@gmail.com>
2021-04-12 11:11:16 +0500

    Update messages.php

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -18,6 +18,7 @@ return [
      'Your PhoneNumber' => 'Your Phonenumber',
      'Your Message' => 'Your Message',
      'CONTACT US' => 'CONTACT US',
+     'Send Message' => 'Send Message',
      'Designed by' => 'Designed by',
      'company was established in Turkmenistan in 2014. From its start, it established itself as the market leader in the production of roasting and packaging of sunflower seeds. For several years, our company has been bringing seeds of various flavors to the consumers of our country, having won the love and trust of consumers. In 2019, it was one of the first in Central Asia to start producing potato, corn, corn wheat, wheat corn and semi-finished pellets for snacks, and also carrying out the production of roasting and packaging chips from them' 
       => 
